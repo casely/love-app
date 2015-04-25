a5c05c0a3bf014d7c3ba86ef3d66b049506bf563
savva <alexeev.savva@gmail.com>
2015-04-25 14:08:25 +0900

    added azure cloud func

---
+++ app/src/main/java/com/example/savqa/love/UserInfo.java
@@ -0,0 +1,11 @@
+package com.example.savqa.love;
+
+/**
+ * Created by savqa on 25.04.2015.
+ */
+public class UserInfo {
+    public String Id;
+    public String Text;
+    public String FirstName;
+    public String LastName;
+}

2872c50095e1c50cccc6f2daff72b96c3d75bfd5
savva <alexeev.savva@gmail.com>
2015-04-25 22:06:21 +0900

    added sign up and in

--- app/src/main/java/com/example/savqa/love/LoginActivity.java
+++ app/src/main/java/com/example/savqa/love/LoginActivity.java
@@ -1,14 +1,24 @@
 package com.example.savqa.love;
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 import com.vk.sdk.VKAccessToken;
 import com.vk.sdk.VKScope;
 import com.vk.sdk.VKSdk;
@@ -29,6 +39,9 @@ public class LoginActivity extends ActionBarActivity {
             VKScope.NOHTTPS
     };
 
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,13 +57,42 @@ public class LoginActivity extends ActionBarActivity {
             }
         });
 
-        if (VKSdk.wakeUpSession()) {
+        findViewById(R.id.signUpBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+            }
+        });
+
+        if (VKSdk.wakeUpSession() || ParseUser.getCurrentUser() != null) {
             startMainActivity();
             return;
         }
 
         String[] fingerprint = VKUtil.getCertificateFingerprint(this, this.getPackageName());
         Log.d("Fingerprint", fingerprint[0]);
+
+        usernameEditText = (EditText) findViewById(R.id.username);
+        passwordEditText = (EditText) findViewById(R.id.password);
+        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (
+                        actionId == EditorInfo.IME_ACTION_UNSPECIFIED) {
+                    login();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        // Set up the submit button click handler
+        Button actionButton = (Button) findViewById(R.id.loginBtn);
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                login();
+            }
+        });
     }
 
     private final VKSdkListener sdkListener = new VKSdkListener() {
@@ -125,6 +167,55 @@ public class LoginActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void login() {
+        String username = usernameEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+        // Validate the log in data
+        boolean validationError = false;
+        StringBuilder validationErrorMessage = new StringBuilder(getString(R.string.error_intro));
+        if (username.length() == 0) {
+            validationError = true;
+            validationErrorMessage.append(getString(R.string.error_blank_username));
+        }
+        if (password.length() == 0) {
+            if (validationError) {
+                validationErrorMessage.append(getString(R.string.error_join));
+            }
+            validationError = true;
+            validationErrorMessage.append(getString(R.string.error_blank_password));
+        }
+        validationErrorMessage.append(getString(R.string.error_end));
+
+        // If there is a validation error, display the error
+        if (validationError) {
+            Toast.makeText(LoginActivity.this, validationErrorMessage.toString(), Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        // Set up a progress dialog
+        final ProgressDialog dialog = new ProgressDialog(LoginActivity.this);
+        dialog.setMessage(getString(R.string.progress_login));
+        dialog.show();
+        // Call the Parse login method
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                dialog.dismiss();
+                if (e != null) {
+                    // Show the error message
+                    Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                } else {
+                    // Start an intent for the dispatch activity
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/savqa/love/MainActivity.java
+++ app/src/main/java/com/example/savqa/love/MainActivity.java
@@ -1,50 +1,34 @@
 package com.example.savqa.love;
 
 import android.content.Intent;
-import android.nfc.Tag;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.microsoft.windowsazure.mobileservices.http.ServiceFilterResponse;
-import com.microsoft.windowsazure.mobileservices.table.TableOperationCallback;
+
+import com.parse.ParseUser;
 import com.vk.sdk.VKSdk;
 import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKApiConst;
 import com.vk.sdk.api.VKRequest;
 import com.vk.sdk.api.VKResponse;
 import com.vk.sdk.api.model.VKApiUser;
-import com.vk.sdk.api.model.VKApiUserFull;
 import com.vk.sdk.api.model.VKList;
-
-import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
-
-import java.net.MalformedURLException;
 
 public class MainActivity extends ActionBarActivity {
 
     private Button logoutButton;
 
-    private String email = VKSdk.getAccessToken().email;
-    private String userid = VKSdk.getAccessToken().userId;
-
-    private MobileServiceClient mClient;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        try {
-            mClient = new MobileServiceClient(
-                    "https://yktloveapp.azure-mobile.net/",
-                    "lTbKOyKilxMTEDhLUZCCiXAnGpknYy10",
-                    this
-            );
 
         VKApi.users().get().executeWithListener(new VKRequest.VKRequestListener() {
 
@@ -52,35 +36,17 @@ public class MainActivity extends ActionBarActivity {
         public void onComplete (VKResponse response) {
                 VKApiUser user = ((VKList<VKApiUser>)response.parsedModel).get(0);
                 TextView t = (TextView)findViewById(R.id.editText);
-                t.setText("Привет " + user.first_name + " " + user.last_name);
+                t.setText("Привет " + user.first_name + " " + user.last_name + user.photo_50);
+
             }
         });
-
-            UserInfo item = new UserInfo();
-
-            item.Text = "privet";
-            item.LastName = "";
-
-            mClient.getTable(UserInfo.class).insert(item, new TableOperationCallback<UserInfo>() {
-                public void onCompleted(UserInfo entity, Exception exception, ServiceFilterResponse response) {
-                    if (exception == null) {
-                        Log.v("My Project", "InsertSuccess");
-                    } else {
-                       Log.d("My Project", "InsertFail");
-                    }
-                }
-            });
-
-        } catch (MalformedURLException e) {
-            new Exception("There was an error creating the Mobile Service. Verify the URL");
-        }
-
 
         logoutButton = (Button) findViewById(R.id.logout_button);
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 VKSdk.logout();
+                ParseUser.logOut();
                 startLoginActivity();
             }
         });
